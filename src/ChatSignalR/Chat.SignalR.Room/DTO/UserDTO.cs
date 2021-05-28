6f520a178313c688f3e4554d79763fb2d9d314ed
Enma <Enma>
2021-05-28 17:06:04 -0500

    base chat signal r

---
+++ src/ChatSignalR/Chat.SignalR.Room/DTO/UserDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chat.SignalR.Room.DTO
+{
+    public class UserDTO
+    {
+        public string ConnectionId { get; set; }
+    }
+}
---
+++ src/ChatSignalR/Chat.SignalR.Room/Hubs/ChatStatus.cs
@@ -0,0 +1,21 @@
+﻿using Chat.SignalR.Room.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chat.SignalR.Room.Hubs
+{
+    public class ChatStatus
+    {
+        public List<string> Connections { get; set; }
+
+        public Dictionary<string, HashSet<UserDTO>> Users = new Dictionary<string, HashSet<UserDTO>>();
+
+        public ChatStatus()
+        {
+            Connections = new List<string>();
+            Users = new Dictionary<string, HashSet<UserDTO>>();
+        }
+    }
+}
